cdf55635d81b8de783b907b99c7ece9dda82fe6f
Patryk Słowikowski <p.slowikowski.97@gmail.com>
2023-03-26 19:59:50 +0200

    Full coverage question.py module

--- question_test.py
+++ question_test.py
@@ -11,3 +11,20 @@ def test_check_answer():
     example_class = Question(1, "Test?", ["tak", "nie"], "a")
     function = example_class.check_answer("a")
     assert function
+
+
+def test__str__():
+    example_class = Question(1, "Test?", ["tak", "nie"], "a")
+    example_print = "----------------------\n"\
+                    "Pytanie nr.1: Test?\n"\
+                    "Oto możliwe odpowiedzi...\n"\
+                    "Odpowiedz a)  tak\n"\
+                    "Odpowiedz b)  nie\n"
+    assert str(example_class) == example_print
+
+
+def test_question_to_dict():
+    example_class = Question(1, "Test?", ["tak", "nie"], "a")
+    question_to_dict = example_class.question_to_dict()
+    example_dict = {"id": 1, "question": "Test?", "answers": ["tak", "nie"], "right_answer": "a"}
+    assert example_dict == question_to_dict
